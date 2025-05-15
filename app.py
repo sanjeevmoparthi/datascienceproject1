0ce457463d2b6fc0d1b28635dffb91141d050175
sanjeevmoparthi <kumarsanjeev7101@gmail.com>
2025-05-15 21:48:40 +0530

    model training

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.datascienceproject1.exception import CustomException
 from src.datascienceproject1.components.data_ingestion import DataIngestion
 from src.datascienceproject1.components.data_ingestion import DataIngestionConfig
 from src.datascienceproject1.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.datascienceproject1.components.model_tranier import MOdelTrainer,MOdelTrainerConfig
 import sys
 
 
@@ -10,14 +11,19 @@ if __name__=="__main__":
     logging.info("The execution has started")
 
     try:
-
+        #data ingestion
         data_ingestion=DataIngestion()
         train_data_path,test_data_path =  data_ingestion.initiate_data_ingestion()
 
-
+        # data transformation
         data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        train_arr,test_arr,_= data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+
+        ### MOdel training:
+        model_trainer = MOdelTrainer()
+        print( model_trainer.initiate_model_trainer(train_arr,test_arr))
 
     except Exception as e:
         logging.info("Custom Exception")

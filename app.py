9fdd97a5c38e911f7f56351f4bcfa6e6ee51daec
sanjeevmoparthi <kumarsanjeev7101@gmail.com>
2025-05-15 09:25:08 +0530

    data ingestion

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from src.datascienceproject1.logger import logging
 from src.datascienceproject1.exception import CustomException
+from src.datascienceproject1.components.data_ingestion import DataIngestion
+from src.datascienceproject1.components.data_ingestion import DataIngestionConfig
 import sys
+
 
 if __name__=="__main__":
     logging.info("The execution has started")
 
     try:
-        a = 1/0
+
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
